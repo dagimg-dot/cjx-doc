da9e7c366b4c69555da3a29431a43de82855afdc
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-13 09:12:54 -0700

    add the main content into cjxbody component

---
+++ src/component/cjxBodySlogan.jsx
@@ -0,0 +1,11 @@
+
+export default function CjxBodySlogan(){
+    return (
+        <div className="flex flex-col justify-center items-center absolute top-55">
+            <p className="text-[90px]  tracking-[-6.42px] ">
+                Effortless JavaFX Environment Setup in VS Code</p>
+                <p className="text-[44px]">Get Coding in minutes</p>
+                <button className="bg-gradient-to-r from-amber-800 to-yellow-500 py-2 px-9 text-white rounded-md">Explore</button>
+            </div>
+    )
+}
