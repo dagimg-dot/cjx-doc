0485c1eeeb18cce6bff583f8dedf12c851638cc0
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-13 09:12:01 -0700

    add the cjxBody component into app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import NavigationBar from "./component/NavigationBar"
+import CjxBody from "./component/cjxBody"
 function App() {
   return (
     <div>
       <NavigationBar/>
+      <CjxBody/>
     </div>
   );
 }
