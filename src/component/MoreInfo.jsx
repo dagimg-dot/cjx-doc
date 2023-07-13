1a657e24f92bae588dccde2111bcb97841aa2341
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-13 13:27:25 -0700

    add contents to more info component

---
+++ src/component/MoreInfo.jsx
@@ -0,0 +1,12 @@
+
+export default function MoreInfo(){
+
+    return(
+        <div className="w-1/3 flex flex-col px-7 text-custom-pink  ">
+            <p className="font-sans text-[52px] font-bold tracking-[-5px] py-5 mt-20">cjx cli</p>
+            <p className="font-consolas leading-6 text-[17px]">cjx is a command line tool that 
+                lets you setup your javaFX development environment in visual studio code.</p>
+
+        </div>
+    )   
+}
