ef9d9a9886a9f37390f28fea721f9fb14a34477d
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-13 11:18:55 -0700

    refine the previous code

--- src/component/cjxBody.jsx
+++ src/component/cjxBody.jsx
@@ -3,7 +3,7 @@ import CjxBodySlogan from './cjxBodySlogan';
 
 export default function CjxBody(){
     return (
-        <div relative className="flex flex-col bg-bodyGradient text-center justify-center text-custom-pink" >
+        <div relative className="flex flex-col bg-bodyGradient text-center justify-center text-custom-pink -mt-10" >
         <p className="blur-xl text-[400px] font-sans">cjx</p>
         <CjxBodySlogan/>
         </div>
