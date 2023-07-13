60ccb32ff87b698c70e7579e36d7728a96cb4055
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-13 13:59:49 -0700

    adjust the padding

--- src/component/MoreInfo.jsx
+++ src/component/MoreInfo.jsx
@@ -2,9 +2,9 @@ import CjxFeatures from "./cjxFeatures";
 export default function MoreInfo(){
 
     return(
-        <div className="w-1/3 flex flex-col px-7 text-custom-pink  ">
+        <div className="w-1/3 flex flex-col px-[40px] text-custom-pink  ">
             <p className="font-sans text-[52px] font-bold tracking-[-5px] py-5 mt-20">cjx cli</p>
-            <p className="font-consolas leading-6 text-[17px]">cjx is a command line tool that 
+            <p className="font-consolas leading-6 text-[18px]">cjx is a command line tool that 
                 lets you setup your javaFX development environment in visual studio code.</p>
             <CjxFeatures/>
 
