00246b6d5c400036b812a4c98d7958cea1e607ea
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-13 11:18:41 -0700

    change the custom gradient in to 180 deg

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
         sans: ["Poppins", "sans-serif"],
       },
       backgroundImage: {
-        "bodyGradient": "linear-gradient(133deg, rgb(10, 10, 12) 19.72%, rgb(11, 11, 53) 100%)",
+        "bodyGradient": "linear-gradient(180deg, rgb(10, 10, 12) 19.72%, rgb(11, 11, 53) 100%)",
       },
       colors:{
         "custom-pink":"#D49E9E",
