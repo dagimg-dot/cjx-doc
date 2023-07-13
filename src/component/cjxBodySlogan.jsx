66c38826fe817520d2e1eed3de9d8e3b1a9de1f4
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-13 11:38:16 -0700

    add font weight to the slogan

--- src/component/cjxBodySlogan.jsx
+++ src/component/cjxBodySlogan.jsx
@@ -1,8 +1,8 @@
 
 export default function CjxBodySlogan(){
     return (
-        <div className="flex flex-col justify-center items-center  text-center absolute top-55 left-80 right-80 text-custom-pink">
-            <p className="text-[60px] tracking-[-6.42px] leading-[60px] ">
+        <div className="flex flex-col justify-center items-center  text-center absolute top-55 left-60 right-60 text-custom-pink">
+            <p className="text-[60px] tracking-[-6.42px] leading-[60px] font-bold">
                 Effortless JavaFX Environment Setup in VS Code</p>
                 <p className="text-[20px] pb-5 pt-3">Get Coding in Minutes!</p>
                 <button className="bg-gradient-to-r from-amber-800 to-yellow-500 py-1 px-9 text-white rounded-md">Explore</button>
