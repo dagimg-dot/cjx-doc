dcea06a743fe8c0b1fbed1ff789b695f5c9fbd0f
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-16 12:42:28 -0700

    reduce the padding as it looks bad

--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -3,18 +3,18 @@ import logo from "../assets/image/cjx-logo.png"
 import 'typeface-poppins'
 export default function NavigationBar() {
   return (
-    <div className="flex justify-between p-5 pl-8 bg-black text-white font-sans">
+    <div className="flex justify-between px-5 pl-8 bg-black text-white font-sans">
       <div>
         <li className="list-none ">
-          <img src={logo} className="w-12 h-12" />
+          <img src={logo} className="w-10 h-10" />
         </li>
       </div>
       <div className="flex py-3">
         <li className="mx-auto list-none px-3">
-          <a href="#">Docs</a>
+          <a href="#" className="text-[11px] text-header-white">DOCS</a>
         </li>
         <li className="mx-auto list-none px-3">
-          <a href="#">Contact</a>
+          <a href="#" className="text-[11px] text-header-white">CONTACT</a>
         </li>
       </div>
       <div className="flex py-3 items-center">
