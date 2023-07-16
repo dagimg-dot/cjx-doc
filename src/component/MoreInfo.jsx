09a084523e868128f56fed8b103505b85e9cfc14
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-16 12:28:16 -0700

    add id to cjx cli heading

--- src/component/MoreInfo.jsx
+++ src/component/MoreInfo.jsx
@@ -4,7 +4,7 @@ export default function MoreInfo(){
 
     return(
         <div className=" flex flex-col px-[40px] text-custom-pink  ">
-            <p className="font-sans text-[52px] font-bold tracking-[-5px] py-5 mt-20">cjx cli</p>
+            <p className="font-sans text-[52px] font-bold tracking-[-5px] py-5 mt-20" id="cjx-heading">cjx cli</p>
             <div className="flex flex-row justify-between">
             <div className="flex flex-col w-1/3">
             <p className="font-consolas leading-6 text-[18px]">cjx is a command line tool that 
