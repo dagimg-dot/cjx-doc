40981e9cb51922e1cd13ae3714a05fa4334e30b4
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-20 12:28:18 +0300

    adjust some responsiveness

--- src/component/cjxBodySlogan.jsx
+++ src/component/cjxBodySlogan.jsx
@@ -5,8 +5,8 @@ export default function CjxBodySlogan(){
         element.scrollIntoView({behavior: "smooth"});
     }
     return (
-        <div className="flex flex-col justify-center items-center  text-center absolute top-55 left-60 right-60 text-custom-pink">
-            <p className="text-[60px] tracking-[-6.42px] leading-[60px] font-bold lg:mx-60">
+        <div className="flex flex-col justify-center items-center  text-center absolute top-55 lg:mx-60 xl:mx-80 text-custom-pink">
+            <p className="text-[60px] tracking-[-6.42px] leading-[60px] font-bold">
                 Effortless JavaFX Environment Setup in VS Code</p>
                 <p className="text-[20px] pb-5 pt-3">Get Coding in Minutes!</p>
                 <a onClick={scrollToMoreInfo} className="bg-gradient-to-r from-amber-800 to-yellow-500 py-1 px-9 text-white rounded-md cursor-default">Explore</a>              
