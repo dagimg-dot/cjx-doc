eb3d2036f4969480b2b16efaf1284c69dd56f615
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-20 13:21:19 +0300

    change class to className

--- src/component/cjxFeatures.jsx
+++ src/component/cjxFeatures.jsx
@@ -2,24 +2,24 @@
 export default function CjxFeatures() {
     return (
         <ul className="flex flex-col text-custom-pink list-none mt-5 font-consolas px-5">
-            <li class="relative pl-5">
-                <span class="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
+            <li className="relative pl-5">
+                <span className="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
                 creates project easily
             </li>
-            <li class="relative pl-5">
-                <span class="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
+            <li className="relative pl-5">
+                <span className="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
                 creates project easily
             </li>
-            <li class="relative pl-5">
-                <span class="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
+            <li className="relative pl-5">
+                <span className="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
                 creates project easily
             </li>
-            <li class="relative pl-5">
-                <span class="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
+            <li className="relative pl-5">
+                <span className="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
                 creates project easily
             </li>
-            <li class="relative pl-5">
-                <span class="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
+            <li className="relative pl-5">
+                <span className="absolute left-0 top-1/2 transform -translate-y-1/2">&#10003;</span>
                 creates project easily
             </li>
         </ul>
