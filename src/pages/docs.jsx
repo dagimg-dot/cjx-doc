5a95602a4ee52ec3a17dd91b05dd6172d0506a70
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-04 17:18:41 +0300

    jit trial

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -4,7 +4,7 @@ export default function () {
   return (
     <div className="bg-bodyGradient w-screen h-screen">
       <NavigationBar />
-      <h1 className="text-white">Docs</h1>
+      <h1 className="text-white">Docs pushed</h1>
     </div>
   );
 }
