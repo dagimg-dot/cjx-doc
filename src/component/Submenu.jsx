5a9f5d188fe40b0a6eb464fdfa1110f667a1b2d8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-05 15:30:47 +0300

    create submenu component

---
+++ src/component/Submenu.jsx
@@ -0,0 +1,7 @@
+export default function Submenu({submenu_name}) {
+    return (
+        <button className="text-white cursor-pointer mx-4 block">
+            {submenu_name}
+        </button>
+    )
+}
