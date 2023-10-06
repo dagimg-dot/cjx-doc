5c6bc346917cc359f859dbe8a13135ad02f12c3c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-06 14:41:05 +0300

    fix bug in new state generation

--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -1,16 +1,16 @@
 const Reducer = (state, action) => {
   switch (action.type) {
     case "PARENT_CLICK":
-      console.log(!state.parent[action.payload].isSelected);
       const payload = action.payload;
       const newState = {
-        ...state.parent,
-        [payload]: {
-          isSelected: !state.parent[payload].isSelected,
-          children: state.parent[payload].children,
+        parent: {
+          ...state.parent,
+          [payload]: {
+            isSelected: !state.parent[payload].isSelected,
+            children: state.parent[payload].children,
+          },
         },
       };
-      console.log(newState);
       return newState;
   }
 };
