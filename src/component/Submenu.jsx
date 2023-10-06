ee40e40847799252ba25fe2e28eaa66a1eb70540
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-06 15:10:45 +0300

    remove onclick prop

--- src/component/Submenu.jsx
+++ src/component/Submenu.jsx
@@ -1,9 +1,7 @@
-export default function Submenu({ submenu_name, clickHandler }) {
+export default function Submenu({ submenu_name }) {
+  console.log(submenu_name);
   return (
-    <button
-      className="text-white cursor-pointer mx-4 block"
-      onclick={() => clickHandler(submenu_name)}
-    >
+    <button className="text-white cursor-pointer mx-4">
       {submenu_name}
     </button>
   );
