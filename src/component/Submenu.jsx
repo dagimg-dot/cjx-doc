f77e9fd6cf2403576b9478d48fe4c4bac7826c85
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-06 15:24:13 +0300

    some ui change

--- src/component/Submenu.jsx
+++ src/component/Submenu.jsx
@@ -1,7 +1,6 @@
 export default function Submenu({ submenu_name }) {
-  console.log(submenu_name);
   return (
-    <button className="text-white cursor-pointer mx-4">
+    <button className="text-white cursor-pointer mx-4 block">
       {submenu_name}
     </button>
   );
