b37bb49723ea2229e716854f8272c09e2c805259
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-06 17:34:59 +0300

    add onClick prop to display in DocBody

--- src/component/Submenu.jsx
+++ src/component/Submenu.jsx
@@ -1,6 +1,14 @@
+import { useContext } from "react";
+import { GlobalContext } from "../store/store";
+
 export default function Submenu({ submenu_name }) {
+  const { handleChildClick } = useContext(GlobalContext);
+
   return (
-    <button className="text-white cursor-pointer mx-4 block">
+    <button
+      className="text-white cursor-pointer mx-4 block"
+      onClick={() => handleChildClick(submenu_name)}
+    >
       {submenu_name}
     </button>
   );
