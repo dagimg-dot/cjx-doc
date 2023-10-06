6e73c565846849ed2efcf85d587a026f9f75c957
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-06 17:36:34 +0300

    add child_click action type and update state

--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -1,17 +1,50 @@
+const findParent = (parent, submenu_name) => {
+  let found = "";
+  const parents = Object.keys(parent);
+
+  parents.map((parent_item) => {
+    if (parent[parent_item].isSelected) {
+      const children = Object.keys(parent[parent_item].children);
+      if (children.some((submenu_item) => submenu_item == submenu_name)) {
+        found = parent_item;
+      }
+    }
+  });
+
+  return found;
+};
+
 const Reducer = (state, action) => {
   switch (action.type) {
     case "PARENT_CLICK":
-      const payload = action.payload;
-      const newState = {
+      const choosenParent = state.parent[action.payload];
+      return {
+        ...state,
         parent: {
           ...state.parent,
-          [payload]: {
-            isSelected: !state.parent[payload].isSelected,
-            children: state.parent[payload].children,
+          [action.payload]: {
+            ...choosenParent,
+            isSelected: !choosenParent.isSelected,
           },
         },
       };
-      return newState;
+
+    case "CHILD_CLICK":
+      const parent = findParent(state.parent, action.payload);
+      const choosenChildState = state.parent[parent].children[action.payload];
+      return {
+        parent: {
+          ...state.parent,
+          [parent]: {
+            children: {
+              ...state.parent[parent].children,
+              [action.payload]: !choosenChildState,
+            },
+            ...state.parent[parent],
+          },
+        },
+        selectedSubMenu: action.payload,
+      };
   }
 };
 
