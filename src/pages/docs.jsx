e62469eeac2e927e7c9653773c4fa0727cbe4378
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-11 04:27:27 -0700

    use left side bar inside docs page

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -1,22 +1,15 @@
 import DocBody from "../component/DocBody";
-import Menu from "../component/Menu";
-import { GlobalContext, GlobalProvider } from "../store/store";
-import { useContext } from "react";
+import { GlobalProvider } from "../store/store";
 import Default from "../layout/Default";
+import LeftSidebar from "../component/LeftSidebar";
 
 export default function () {
-  const { parent } = useContext(GlobalContext);
-
-  const parents = Object.keys(parent);
-
   return (
     <div className="bg-bodyGradient w-screen h-screen">
       <Default>
         <h1 className="text-white">Docs</h1>
         <GlobalProvider>
-          {parents.map((menu_item) => (
-            <Menu key={menu_item} menu_name={menu_item} />
-          ))}
+          <LeftSidebar />
           <DocBody />
         </GlobalProvider>
       </Default>
