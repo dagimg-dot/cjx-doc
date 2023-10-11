ddc1ffa2932b9566131de2fdb435b828cdf1931c
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-11 06:58:26 -0700

    add more parent for testing

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,11 +2,13 @@ import { createContext, useReducer } from "react";
 import { ACTION_TYPE } from "../utils/types";
 import Reducer from "./Reducer";
 
-const menu_names = ["Parent 1", "Parent 2", "Parent 3"];
+const menu_names = ["Parent 1", "Parent 2", "Parent 3","Parent 4","Parent 5"];
 const submenu_names = ["Child 1", "Child 2", "Child 3", "Child 4"];
 const submenu_names2 = ["Child 5", "Child 6", "Child 7", "Child 8"];
 const submenu_names3 = ["Child 9", "Child 10", "Child 11", "Child 12"];
-const parent_submenu = [submenu_names, submenu_names2, submenu_names3];
+const submenu_names4 = ["Child 9", "Child 10", "Child 11", "Child 12"];
+const submenu_names5 = ["Child 9", "Child 10", "Child 11", "Child 12"];
+const parent_submenu = [submenu_names, submenu_names2, submenu_names3, submenu_names4, submenu_names5];
 
 const initialState = {
   parent: {},
