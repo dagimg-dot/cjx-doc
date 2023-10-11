f8efcc64766254b15fc12d2ed094c202528cc4a3
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-11 05:19:40 -0700

    center components in their own container

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -7,16 +7,15 @@ export default function () {
   return (
     <div className="bg-bodyGradient w-screen h-screen">
       <Default>
-        <h1 className="text-white">Docs</h1>
         <GlobalProvider>
           <div className="grid grid-cols-4 ">
-            <div className="flex flex-col">
+            <div className="flex flex-col border-r border-custom-white items-center">
               <LeftSidebar />
             </div>
             <div className="flex flex-col border-r border-custom-white col-span-2">
               <DocBody />
             </div>
-            <div className="text-white">scroll over </div>
+            <div className="text-white flex flex-col items-center">scroll over </div>
           </div>
         </GlobalProvider>
       </Default>
