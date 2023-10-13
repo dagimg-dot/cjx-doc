dda3f4cf830c10815117d8abff2c0b5c29f85d96
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-13 17:36:14 +0300

    additional parents for testing

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,13 +2,27 @@ import { createContext, useReducer } from "react";
 import { ACTION_TYPE } from "../utils/types";
 import Reducer from "./Reducer";
 
-const menu_names = ["Parent 1", "Parent 2", "Parent 3","Parent 4","Parent 5"];
+const menu_names = [
+  "Parent 1",
+  "Parent 2",
+  "Parent 3",
+  "Parent 4",
+  "Parent 5",
+  "Parent 6",
+  "Parent 7",
+];
 const submenu_names = ["Child 1", "Child 2", "Child 3", "Child 4"];
 const submenu_names2 = ["Child 5", "Child 6", "Child 7", "Child 8"];
 const submenu_names3 = ["Child 9", "Child 10", "Child 11", "Child 12"];
-const submenu_names4 = ["Child 9", "Child 10", "Child 11", "Child 12"];
-const submenu_names5 = ["Child 9", "Child 10", "Child 11", "Child 12"];
-const parent_submenu = [submenu_names, submenu_names2, submenu_names3, submenu_names4, submenu_names5];
+const parent_submenu = [
+  submenu_names,
+  submenu_names2,
+  submenu_names3,
+  submenu_names3,
+  submenu_names3,
+  submenu_names3,
+  submenu_names3,
+];
 
 const initialState = {
   parent: {},
@@ -18,7 +32,7 @@ const initialState = {
 menu_names.map((menu_item) => {
   initialState.parent = {
     ...initialState.parent,
-    [menu_item]: { isSelected: false, children: {} },
+    [menu_item]: { isSelected: true, children: {} },
   };
 });
 
