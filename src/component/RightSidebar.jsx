33c181d3c7b678a6410deefe7870b7a5e6e907ab
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-20 15:30:08 +0300

    applying clean code principles

--- src/component/RightSidebar.jsx
+++ src/component/RightSidebar.jsx
@@ -2,13 +2,15 @@ import { GlobalContext } from "../store/store";
 import { useContext, useEffect } from "react";
 
 const RightSidebar = () => {
-  const { selectedSubMenu} = useContext(GlobalContext);
-  console.log(selectedSubMenu)
-  
+  const { selectedSubMenu } = useContext(GlobalContext);
+  const subChildren = Object.keys(selectedSubMenu.sub_children);
+
   return (
-    <div className = "mx-10">
-      {Object.keys(selectedSubMenu.sub_children).map((sub_child) => (
-        <p className="my-2 px-2 border-l border-custom-pink" key={sub_child}>{sub_child}</p>
+    <div className="mx-10">
+      {subChildren.map((sub_child) => (
+        <p className="my-2 px-2 border-l border-custom-pink" key={sub_child}>
+          {sub_child}
+        </p>
       ))}
     </div>
   );
