10946283ddfa509f111b70250d54cadb28adcc91
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-10-20 13:11:30 +0300

    load child name in the doc body

--- src/component/DocBody.jsx
+++ src/component/DocBody.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 
 const DocBody = () => {
   const { selectedSubMenu } = useContext(GlobalContext);
-  return <div className="text-white text-[20px]">{selectedSubMenu}</div>;
+  return <div className="text-white text-[20px]">{selectedSubMenu.name}</div>;
 };
 
 export default DocBody;
