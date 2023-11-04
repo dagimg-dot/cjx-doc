4e9458184231c1eafaccc866db8e4dd152c8ae58
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-04 15:59:17 +0300

    add config to include .md assets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cjx-doc/'
+  base: '/cjx-doc/',
+  assetsInclude:"**/*.md"
 })
