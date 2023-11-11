e92abd4b2fcee28df581ad90bea67535753df816
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-11 22:24:17 +0300

    create selected submenu to component mapper

---
+++ src/utils/docPages.jsx
@@ -0,0 +1,9 @@
+import Prerequisites from "../docs/Prerequisites";
+import SettingUp from "../docs/SettingUp";
+
+const DocPages = {
+  Prerequisites: <Prerequisites />,
+  "Setting Up the Environment": <SettingUp />,
+};
+
+export default DocPages;
