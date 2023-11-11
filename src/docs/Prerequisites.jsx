a5061eda1badf3c4d70cbf9ce371c64f945289e3
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-11 22:23:04 +0300

    create sample doc pages for testing

---
+++ src/docs/Prerequisites.jsx
@@ -0,0 +1,5 @@
+const Prerequisites = () => {
+  return <div className="text-white">Prerequisites</div>;
+};
+
+export default Prerequisites;
---
+++ src/docs/SettingUp.jsx
@@ -0,0 +1,5 @@
+const SettingUp = () => {
+  return <div className="text-white">Setting Up the Environment</div>;
+};
+
+export default SettingUp;
