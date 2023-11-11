cc7e0a32c13bfc48a4cb1d7287613e706df1d12e
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-11 22:39:16 +0300

    add title for right side bar

--- src/component/RightSidebar.jsx
+++ src/component/RightSidebar.jsx
@@ -6,7 +6,8 @@ const RightSidebar = () => {
   const subChildren = Object.keys(selectedSubMenu.sub_children);
 
   return (
-    <div className="mx-10">
+    <div className="mx-2">
+      <h2>On this page</h2>
       {subChildren.map((sub_child) => (
         <p className="my-2 px-2 border-l border-custom-pink" key={sub_child}>
           {sub_child}
