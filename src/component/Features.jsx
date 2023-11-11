675ed16fdf739e055abfa3e6d32018e379110faf
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-11 23:07:51 +0300

    clean up component using the stored data

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -1,36 +1,16 @@
+import { features } from "../utils/data";
+
 export default function Features() {
   return (
     <ul className="flex flex-col text-custom-pink list-none mt-5 font-consolas px-5">
-      <li className="relative pl-5">
-        <span className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          &#10003;
-        </span>
-        creates project easily
-      </li>
-      <li className="relative pl-5">
-        <span className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          &#10003;
-        </span>
-        creates project easily
-      </li>
-      <li className="relative pl-5">
-        <span className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          &#10003;
-        </span>
-        creates project easily
-      </li>
-      <li className="relative pl-5">
-        <span className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          &#10003;
-        </span>
-        creates project easily
-      </li>
-      <li className="relative pl-5">
-        <span className="absolute left-0 top-1/2 transform -translate-y-1/2">
-          &#10003;
-        </span>
-        creates project easily
-      </li>
+      {features.map((feature) => (
+        <li className="relative pl-5">
+          <span className="absolute left-0 top-1/2 transform -translate-y-1/2">
+            &#10003;
+          </span>
+          {feature}
+        </li>
+      ))}
     </ul>
   );
 }
