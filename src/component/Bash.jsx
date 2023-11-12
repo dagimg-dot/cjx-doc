400ca855cf88c38f757d27873decb60873133cb7
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-12 21:40:26 +0300

    use the Terminal layout to show the terminal

--- src/component/Bash.jsx
+++ src/component/Bash.jsx
@@ -1,6 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import useAnimation from "../hooks/useAnimation";
+import Terminal from "../layout/Terminal";
 
 export default function Bash() {
   const bashCommands = [
@@ -14,24 +13,8 @@ export default function Bash() {
   const displayedCommand = useAnimation(bashCommands);
 
   return (
-    <div className="flex flex-col bg-black w-[350px] h-[250px] font-consolas text-bash-white mt-18 mr-60 border border-custom-white rounded-lg">
-      <div className=" flex border-b border-custom-white  py-4 mb-2">
-        <FontAwesomeIcon
-          icon={faCircle}
-          className="text-red-500 text-xs mt-1 ml-5"
-        />
-        <FontAwesomeIcon
-          icon={faCircle}
-          className="text-yellow-500 text-xs mt-1 ml-2"
-        />
-        <FontAwesomeIcon
-          icon={faCircle}
-          className="text-green-500 text-xs mt-1 ml-2 mr-6"
-        />
-
-        <p className="text-center justify-center ml-16 text-[12px]">bash</p>
-      </div>
+    <Terminal>
       <p className="text-[12px] mt-1 tracking-[1.5px] ml-4">{`$ ${displayedCommand}`}</p>
-    </div>
+    </Terminal>
   );
 }
