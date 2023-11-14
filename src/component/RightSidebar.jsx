43abb0c6ef8a37b095c309eefd6057dbf454653f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 17:19:48 +0300

    small UI changes

--- src/component/RightSidebar.jsx
+++ src/component/RightSidebar.jsx
@@ -9,10 +9,10 @@ export default function RightSidebar() {
     <div className="mx-2">
       {subChildren.length != 0 && (
         <div>
-          <h2 className="mb-6">On this page</h2>
+          <h2 className="mb-4 font-bold text-lg">On this page</h2>
           {subChildren.map((sub_child) => (
             <p
-              className="my-2 px-2 border-l border-custom-pink"
+              className="my-2 px-2 border-l-2 border-custom-pink"
               key={sub_child}
             >
               {sub_child}
