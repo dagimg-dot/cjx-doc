bb4379e935010690bfae8cd3e2e491c1c229aa47
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 22:26:34 +0300

    refactor more info component

--- src/component/MoreInfo.jsx
+++ src/component/MoreInfo.jsx
@@ -3,24 +3,25 @@ import Features from "./Features";
 
 export default function MoreInfo() {
   return (
-    <div className=" flex flex-col px-[40px] text-custom-pink  ">
-      <p
-        className="font-sans text-[52px] font-bold tracking-[-5px] py-5 mt-20"
-        id="cjx-heading"
-      >
-        cjx cli
-      </p>
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-col w-1/3">
-          <p className="font-consolas leading-6 text-[18px]">
-            cjx is a command line tool that lets you setup your javaFX
-            development environment in visual studio code.
-          </p>
-
-          <Features />
+    <div className="flex px-20 text-custom-pink items-center pt-10">
+      <div className="flex flex-col">
+        <p
+          className="font-sans text-[52px] font-bold tracking-[-3px] py-5"
+          id="cjx-heading"
+        >
+          cjx cli
+        </p>
+        <div className="flex justify-between">
+          <div className="flex flex-col w-1/2">
+            <p className="font-consolas leading-6 text-[18px]">
+              cjx is a command line tool that lets you setup your javaFX
+              development environment in visual studio code.
+            </p>
+            <Features />
+          </div>
         </div>
-        <Bash />
       </div>
+      <Bash />
     </div>
   );
 }
