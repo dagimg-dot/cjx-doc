3758bbe72aa990ee7c4832f455241290ec826dfa
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 22:27:43 +0300

    refactor terminal layout

--- src/layout/Terminal.jsx
+++ src/layout/Terminal.jsx
@@ -6,22 +6,24 @@ export default function Terminal({ style, children }) {
     <div
       className={`flex flex-col bg-black font-consolas text-bash-white mt-6 mb-6 border border-custom-white rounded-lg ${style}`}
     >
-      <div className=" flex border-b border-custom-white  py-4 mb-2">
-        <FontAwesomeIcon
-          icon={faCircle}
-          className="text-red-500 text-xs mt-1 ml-5"
-        />
-        <FontAwesomeIcon
-          icon={faCircle}
-          className="text-yellow-500 text-xs mt-1 ml-2"
-        />
-        <FontAwesomeIcon
-          icon={faCircle}
-          className="text-green-500 text-xs mt-1 ml-2 mr-6"
-        />
-        <p className="text-center justify-center ml-16 text-[12px]">bash</p>
+      <div className="flex border-b border-custom-white  py-2 mb-2">
+        <div>
+          <FontAwesomeIcon
+            icon={faCircle}
+            className="text-red-500 text-xs mt-1 ml-5"
+          />
+          <FontAwesomeIcon
+            icon={faCircle}
+            className="text-yellow-500 text-xs mt-1 ml-2"
+          />
+          <FontAwesomeIcon
+            icon={faCircle}
+            className="text-green-500 text-xs mt-1 ml-2 mr-6"
+          />
+        </div>
+        <p className="text-sm flex-grow text-center pr-10">bash</p>
       </div>
-      {children}
+      <div className="p-4 flex gap-4">{"$ "}{children}</div>
     </div>
   );
 }
