c8292ba3be245b31118b244770b95c4ef188897d
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 17:25:13 +0300

    remove rules in path

--- src/component/Check.jsx
+++ src/component/Check.jsx
@@ -8,8 +8,6 @@ function Check() {
       fill="#C5C5C5"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
         d="M14.431 3.323l-8.47 10-.79-.036-3.35-4.77.818-.574 2.978 4.24 8.051-9.506.764.646z"
       />
     </svg>
