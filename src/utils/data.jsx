6e81abfd642b03006118485dfe0b1d6afa4ed65a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 17:22:05 +0300

    add prerequsite link object for more readable code

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -6,4 +6,16 @@ const features = [
   "create project easily",
 ];
 
-export { features };
+const prerequisiteLinks = {
+  "Java 11 or higher":
+    "https://www.oracle.com/java/technologies/javase-jdk11-downloads.html",
+  "VS Code": "https://code.visualstudio.com/download",
+  "VS Code Java Extension Pack":
+    "https://marketplace.visualstudio.com/items?itemName=vscjava.vscode-java-pack",
+  "JavaFX SDK": "https://gluonhq.com/products/javafx/",
+  Git: "https://git-scm.com/downloads",
+  "Scene Builder Extenstion for VS Code":
+    "https://marketplace.visualstudio.com/items?itemName=bilalekrem.scenebuilderextension",
+};
+
+export { features, prerequisiteLinks };
