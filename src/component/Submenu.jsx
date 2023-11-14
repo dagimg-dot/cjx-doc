f6e64837ac1b169585cfbceb93e6ca9923944953
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 23:28:23 +0300

    change color of submenu when selected

--- src/component/Submenu.jsx
+++ src/component/Submenu.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import { GlobalContext } from "../store/store";
 
 export default function Submenu({ submenu_name }) {
-  const { handleChildClick } = useContext(GlobalContext);
+  const { selectedSubMenu, handleChildClick } = useContext(GlobalContext);
+  const selectedColor = submenu_name === selectedSubMenu.name ? "border-l-2 pl-2 text-[#bcbcbc] border-[#bcbcbc]" : ""
 
   return (
     <button
-      className="text-white cursor-pointer block"
+      className={`text-white cursor-pointer block ${selectedColor}`}
       onClick={() => handleChildClick(submenu_name)}
     >
       {submenu_name}
