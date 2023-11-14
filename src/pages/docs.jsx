dcde56dcbebce72cebb32fd14e72b29f536c7094
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 22:53:50 +0300

    remove unused properties

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -7,10 +7,10 @@ import RightSidebar from "../component/RightSidebar";
 export default function () {
 
   return (
-    <div className="bg-bodyGradient h-screen relative">
+    <div className="bg-bodyGradient h-screen">
       <Default>
         <GlobalProvider>
-          <div className="grid grid-cols-6 h-full w-full absolute pt-28 px-4 text-[13px]">
+          <div className="grid grid-cols-6 h-full w-full pt-28 px-28 text-[13px]">
             <div className="flex flex-col border-r border-custom-white overflow-y-auto">
               <LeftSidebar />
             </div>
