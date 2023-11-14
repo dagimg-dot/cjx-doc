8d0576f40078b87c57728751b1c228c7dba66ae4
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 23:37:32 +0300

    change grid layout to 1-3-1

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -5,20 +5,19 @@ import LeftSidebar from "../component/LeftSidebar";
 import RightSidebar from "../component/RightSidebar";
 
 export default function () {
-
   return (
     <div className="bg-bodyGradient h-screen">
       <Default>
         <GlobalProvider>
-          <div className="grid grid-cols-6 h-full w-full pt-28 px-28 text-[13px]">
+          <div className="grid grid-cols-5 h-full w-full pt-28 px-28 text-[13px]">
             <div className="flex flex-col border-r border-custom-white overflow-y-auto">
               <LeftSidebar />
             </div>
-            <div className="flex flex-col border-r border-custom-white col-span-4 mx-8 text-white text-[14px] overflow-y-auto">
+            <div className="flex flex-col border-r border-custom-white col-span-3 mx-8 text-white text-[14px] overflow-y-auto">
               <DocBody />
             </div>
             <div className="text-white flex flex-col">
-              <RightSidebar/>
+              <RightSidebar />
             </div>
           </div>
         </GlobalProvider>
