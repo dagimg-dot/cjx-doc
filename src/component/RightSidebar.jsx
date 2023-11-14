1be317dc67bb901d3e2cb73f66c7aaffc8e8b0eb
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 23:07:26 +0300

    increase gap between subchildren

--- src/component/RightSidebar.jsx
+++ src/component/RightSidebar.jsx
@@ -8,7 +8,7 @@ export default function RightSidebar() {
   return (
     <div className="mx-2">
       {subChildren.length != 0 && (
-        <div>
+        <div className="flex flex-col">
           <h2 className="mb-4 font-bold text-lg">On this page</h2>
           {subChildren.map((sub_child) => (
             <p
