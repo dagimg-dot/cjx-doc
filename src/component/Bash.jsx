09f9950aa0f8ebbc6bd4f6712b3bf41aec70cc5e
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 22:25:44 +0300

    pass defined width and height for home page terminal

--- src/component/Bash.jsx
+++ src/component/Bash.jsx
@@ -4,8 +4,9 @@ import Terminal from "../layout/Terminal";
 export default function Bash() {
   const bashCommands = [
     "cjx init",
-    "cjx setup",
+    "cjx setup <JavaFX SDK directory>",
     "cjx create simple <Project Name>",
+    "cjx create jfxml <Project Name>",
     "cjx clone <Project url>",
     "cjx doctor",
   ];
@@ -13,8 +14,8 @@ export default function Bash() {
   const displayedCommand = useAnimation(bashCommands);
 
   return (
-    <Terminal>
-      <p className="text-[12px] mt-1 tracking-[1.5px] ml-4">{`$ ${displayedCommand}`}</p>
+    <Terminal style={"w-[550px] h-[300px]"}>
+      <p className="text-sm tracking-[1.5px] mt-[1.5px]">{`${displayedCommand}`}</p>
     </Terminal>
   );
 }
