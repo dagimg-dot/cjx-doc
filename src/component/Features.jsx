dbd8bfc87744930a75b6060dc795eef08c9c908c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-14 22:25:52 +0300

    use the new check component

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -1,13 +1,12 @@
 import { features } from "../utils/data";
+import Check from "./Check";
 
 export default function Features() {
   return (
     <ul className="flex flex-col text-custom-pink list-none mt-5 font-consolas px-5">
       {features.map((feature) => (
-        <li className="relative pl-5">
-          <span className="absolute left-0 top-1/2 transform -translate-y-1/2">
-            &#10003;
-          </span>
+        <li className="relative pl-2 flex gap-2 items-center">
+          <Check />
           {feature}
         </li>
       ))}
