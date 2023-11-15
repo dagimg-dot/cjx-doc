0aa6e581b75aacb9d603ec3bb6394fb916c1a95a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-15 10:33:47 +0300

    add key prop for better rendering

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -5,7 +5,7 @@ export default function Features() {
   return (
     <ul className="flex flex-col text-custom-pink list-none mt-5 font-consolas px-5">
       {features.map((feature) => (
-        <li className="relative pl-2 flex gap-2 items-center">
+        <li className="relative pl-2 flex gap-2 items-center" key={feature}>
           <Check />
           {feature}
         </li>
