ec6cebbe8e054f2790ef5bb859127ebd75b53bda
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-16 16:00:04 +0300

    import and use the findParent method

--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -1,19 +1,5 @@
 import { ACTION_TYPE } from "../utils/types";
-
-const findParent = (parent, submenu_name) => {
-  let found = "";
-  const parents = Object.keys(parent);
-  parents.map((parent_item) => {
-    // if (parent[parent_item].isSelected) {
-    const children = Object.keys(parent[parent_item].children);
-    if (children.some((submenu_item) => submenu_item == submenu_name)) {
-      found = parent_item;
-    }
-    // }
-  });
-
-  return found;
-};
+import findParent from "../utils/findParent";
 
 const Reducer = (state, action) => {
   switch (action.type) {
