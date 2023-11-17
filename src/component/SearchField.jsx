0a8d20b10d3e11468259b949c796bc40e0204a7d
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-17 05:20:29 +0300

    use the useOutsideClick hook to remove search results when input focus is lost

--- src/component/SearchField.jsx
+++ src/component/SearchField.jsx
@@ -1,13 +1,18 @@
 import Search from "./Search";
 import { GlobalContext } from "../store/store";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import useSearch from "../hooks/useSearch";
 import findParent from "../utils/findParent";
+import useOutsideClick from "../hooks/useOutSideClick";
 
 const SearchField = () => {
   const { parent, handleChildClick, handleParentClick } =
     useContext(GlobalContext);
   const [results, searchToken, setSearchToken] = useSearch();
+  const [isShown, setIsShown] = useState(true);
+  const ref = useOutsideClick(() => {
+    setIsShown(false);
+  });
 
   const handlerResultClick = (result) => {
     setSearchToken("");
@@ -20,28 +25,31 @@ const SearchField = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="search-field flex items-center gap-2 text-white text-sm font-normal py-1 px-2 border-2 border-lime-500 from-amber-800 to-yellow-500 rounded-lg">
-        <Search />
-        <input
-          type="text"
-          className="bg-transparent focus:outline-none"
-          onChange={(event) => setSearchToken(event.target.value)}
-          value={searchToken}
-        />
-      </div>
-      {searchToken !== "" && results.length > 0 && (
-        <div className="mt-12  bg-slate-800/90 rounded-lg list-none p-4 absolute w-[219px]">
-          {results.map((result) => (
-            <li
-              key={result}
-              className="hover:bg-slate-400 cursor-pointer p-2 rounded-md"
-              onClick={() => handlerResultClick(result)}
-            >
-              {result}
-            </li>
-          ))}
+      <div ref={ref}>
+        <div className="search-field flex items-center gap-2 text-white text-sm font-normal py-1 px-2 border-2 border-lime-500 from-amber-800 to-yellow-500 rounded-lg">
+          <Search />
+          <input
+            type="text"
+            className="bg-transparent focus:outline-none"
+            onChange={(event) => setSearchToken(event.target.value)}
+            onFocus={() => setIsShown(true)}
+            value={searchToken}
+          />
         </div>
-      )}
+        {isShown && results.length > 0 && (
+          <div className="mt-4  bg-slate-800/90 rounded-lg list-none p-4 absolute w-[219px]">
+            {results.map((result) => (
+              <li
+                key={result}
+                className="hover:bg-slate-400 cursor-pointer p-2 rounded-md"
+                onClick={() => handlerResultClick(result)}
+              >
+                {result}
+              </li>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
