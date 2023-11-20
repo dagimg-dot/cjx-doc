6ebac35ad6d9df14a1ce561288654efc84361138
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-20 12:07:25 +0300

    destructure return of useHash

--- src/component/Submenu.jsx
+++ src/component/Submenu.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import useHash from "../hooks/useHash";
 
 const Submenu = ({ submenu_name }) => {
-  const currentPage = useHash();
+  const [currentPage] = useHash();
   const selectedColor =
     submenu_name === currentPage
       ? "border-l-2 pl-2 text-[#bcbcbc] border-[#bcbcbc]"
