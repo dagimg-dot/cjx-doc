286ad6cdc51aaece3cb6bdcd7a5bbaed744b4f74
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-21 01:16:37 +0300

    finish prev and next button with disabled state and styles

--- src/component/DocBody.jsx
+++ src/component/DocBody.jsx
@@ -2,39 +2,46 @@ import DocPages from "../utils/docPages";
 import useHash from "../hooks/useHash";
 import { useNavigate } from "react-router-dom";
 import { docPagesList } from "../utils/data";
+import { useEffect, useState } from "react";
+import Button from "./Button";
 
 const DocBody = () => {
   const [currentPage] = useHash();
   const navigate = useNavigate();
+  const [page, setPage] = useState(currentPage);
+  const [isPrevBtnDisabled, setPrevBtnDisabled] = useState(false);
+  const [isNextBtnDisabled, setNextBtnDisabled] = useState(false);
 
-  const currentDoc = DocPages[currentPage];
+  useEffect(() => {
+    setPage(currentPage);
+    setPrevBtnDisabled(getIndex(currentPage) === 0);
+    setNextBtnDisabled(getIndex(currentPage) === docPagesList.length - 1);
+  }, [currentPage]);
+
+  const currentDoc = DocPages[page];
+
+  const getIndex = (_page = page) => docPagesList.indexOf(_page);
 
   const handlePrev = () => {
-    if (docPagesList.indexOf(currentPage) == 0) return;
-    navigate(`#${docPagesList[docPagesList.indexOf(currentPage) - 1]}`);
+    const prevPage = docPagesList[getIndex() - 1];
+    navigate(`#${prevPage}`);
   };
 
   const handleNext = () => {
-    if (docPagesList.indexOf(currentPage) == docPagesList.length - 1) return;
-    navigate(`#${docPagesList[docPagesList.indexOf(currentPage) + 1]}`);
+    const nextPage = docPagesList[getIndex() + 1];
+    navigate(`#${nextPage}`);
   };
 
   return (
     <div className="leading-6 flex flex-col justify-between h-full">
       <div className="flex-grow">{currentDoc}</div>
-      <div className="flex justify-between mr-8 pb-12">
-        <button
-          className="bg-gradient-to-r from-amber-800 to-yellow-500 py-1 px-6 text-white rounded-md cursor-pointer"
-          onClick={handlePrev}
-        >
+      <div className="flex justify-between mr-8 pb-8">
+        <Button onClick={handlePrev} disabled={isPrevBtnDisabled}>
           &larr; Prev
-        </button>
-        <button
-          className="bg-gradient-to-r from-amber-800 to-yellow-500 py-1 px-6 text-white rounded-md cursor-pointer"
-          onClick={handleNext}
-        >
+        </Button>
+        <Button onClick={handleNext} disabled={isNextBtnDisabled}>
           Next &rarr;
-        </button>
+        </Button>
       </div>
     </div>
   );
