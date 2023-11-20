2b7ea28d7750e17c8968bf93c24f94dcdc9ed416
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-20 17:06:59 +0300

    make default page to fisrt page and change dependency array

--- src/hooks/useHash.jsx
+++ src/hooks/useHash.jsx
@@ -1,14 +1,27 @@
 import { useLocation } from "react-router-dom";
+import findParent from "../utils/findParent";
+import { useContext, useEffect } from "react";
+import { GlobalContext } from "../store/store";
 
 const decodeURLHash = (hash) => {
-  return hash.split("%20").join(" ");
+  return hash.split("%20").join(" ").split("#").pop();
 };
 
 const useHash = () => {
+  const { parent, handleParentClick } = useContext(GlobalContext);
   const location = useLocation();
-  const hash = decodeURLHash(location.hash);
-  const currentPage = hash.split("#").pop();
-  return currentPage;
+  const currentPage = location.hash
+    ? decodeURLHash(location.hash)
+    : "Prerequisites";
+  const { parentName, isSelected } = findParent(parent, currentPage);
+
+  useEffect(() => {
+    if (!isSelected) {
+      handleParentClick(parentName);
+    }
+  }, [currentPage, location, location.hash]);
+
+  return [currentPage, parent[parentName]];
 };
 
 export default useHash;
