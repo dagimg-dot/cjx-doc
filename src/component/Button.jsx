bc662fb3548ad156347f1faa0be79b0bce61dc2c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-21 01:15:45 +0300

    create a new button component

---
+++ src/component/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ style, onClick, disabled, children }) => {
+  return (
+    <button
+      className={`bg-amber-600 py-1 px-6 text-white rounded-md cursor-pointer disabled:bg-slate-500 disabled:cursor-not-allowed + ${style}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
