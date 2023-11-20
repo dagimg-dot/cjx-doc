c807220fea621d4e48c9cbcc6e263297573558db
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-20 22:38:33 +0300

    some responsiveness changes 😒

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -3,7 +3,7 @@ import Slogan from "./Slogan";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col text-center justify-center text-custom-pink -mt-10 absolute w-full ">
+    <div className="flex flex-col text-center justify-center text-custom-pink -mt-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-full ">
       <p className="blur-xl text-[400px] font-sans md:text-2xl maxsm:text-[300px]">cjx</p>
       <Slogan />
     </div>
