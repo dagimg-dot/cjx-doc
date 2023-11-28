77f71e100db200af8cfd6ba32fde432f71dd7c67
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-28 12:45:33 -0800

    add marigin to the vertical direction

--- src/component/Hamburger.jsx
+++ src/component/Hamburger.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { FaBars } from 'react-icons/fa'
 const Hamburger = ({onClick}) => {
   return (
-    <>
-    <FaBars className='text-2xl text-white' onClick={onClick}/>
-    </>
+    <div className='flex'>
+    <FaBars className='text-2xl text-lime-500 mx-5 my-10' onClick={onClick}/>
+    </div>
   )
 }
 
