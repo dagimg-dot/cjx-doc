008eec011d19730abee8ba94a457549e79069156
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-30 23:43:11 +0300

    fix: show only on smaller screens

--- src/component/Hamburger.jsx
+++ src/component/Hamburger.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { FaBars } from 'react-icons/fa'
 const Hamburger = ({onClick}) => {
   return (
-    <>
+    <div className='hidden mdsm:block'>
     <FaBars className='text-2xl text-lime-500 mx-5 my-10' onClick={onClick}/>
-    </>
+    </div>
   )
 }
 
--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -6,63 +6,55 @@ import "../css/NavigationBarHover.css";
 import { PAGES } from "../utils/types";
 import SearchField from "./SearchField";
 import SideNavBar from "./SideNavBar";
-import { useEffect,useState } from "react";
 
 const NavigationBar = () => {
-  const [isTabletWidth,setTabletWidth] = useState(false)
   const { pathname } = useLocation();
   const isDocPage = pathname.split("/").pop() === PAGES.DOCS;
   const paddingLeft = isDocPage ? "pl-56" : "";
-  useEffect(()=> {
-    console.log(window.innerWidth)
-    if(window.innerWidth<=640)
-      setTabletWidth(!isTabletWidth)
-  },[window.innerWidth])
+
   return (
     <div>
-      {!isTabletWidth ? (
-        <div className="flex justify-between px-28 minmd:px-8 py-4 bg-header-black text-white font-sans z-50 absolute w-full font-bold text-sm">
-          <div>
-            <li className="list-none ">
-              <Link to="/">
-                <img src={logo} className="w-12 h-12" />
-              </Link>
-            </li>
-          </div>
-          <div className={`flex py-3 items-center ${paddingLeft}`}>
-            <li className="mx-auto list-none px-3">
-              <Link
-                to="/docs"
-                className="text-header-white tracking-[1.5px] relative nav-bar "
-              >
-                DOCS<span className=""></span>
-              </Link>
-            </li>
-            <li className="mx-auto list-none px-3">
-              <Link
-                to="/contact"
-                className="text-header-white tracking-[1.5px] relative nav-bar"
-              >
-                CONTACT<span className=""></span>
-              </Link>
-            </li>
-          </div>
-          <div className="flex py-3 px-5 items-center gap-5">
-            {isDocPage && <SearchField />}
-            <li className="list-none">
-              <a
-                href="https://github.com/dagimg-dot/cjx-cli-tool"
-                target={"_blank"}
-                title={"Github Link"}
-              >
-                <FaGithub className="scale-[2]" />
-              </a>
-            </li>
-          </div>
+      <SideNavBar />
+      <div className="mdsm:hidden flex justify-between px-28 minmd:px-8 py-4 bg-header-black text-white font-sans z-50 absolute w-full font-bold text-sm">
+        <div>
+          <li className="list-none ">
+            <Link to="/">
+              <img src={logo} className="w-12 h-12" />
+            </Link>
+          </li>
         </div>
-      ) : (
-        <SideNavBar />
-      )}
+        <div className={`flex py-3 items-center ${paddingLeft}`}>
+          <li className="mx-auto list-none px-3">
+            <Link
+              to="/docs"
+              className="text-header-white tracking-[1.5px] relative nav-bar "
+            >
+              DOCS<span className=""></span>
+            </Link>
+          </li>
+          <li className="mx-auto list-none px-3">
+            <Link
+              to="/contact"
+              className="text-header-white tracking-[1.5px] relative nav-bar"
+            >
+              CONTACT<span className=""></span>
+            </Link>
+          </li>
+        </div>
+        <div className="flex py-3 px-5 items-center gap-5">
+          {isDocPage && <SearchField />}
+          <li className="list-none">
+            <a
+              href="https://github.com/dagimg-dot/cjx-cli-tool"
+              target={"_blank"}
+              title={"Github Link"}
+            >
+              <FaGithub className="scale-[2]" />
+            </a>
+          </li>
+        </div>
+      </div>
+      )
     </div>
   );
 };
