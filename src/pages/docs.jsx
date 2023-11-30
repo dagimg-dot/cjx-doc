73821300908638bc5a74f21348b5e36f9ae6fab0
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-01 01:34:22 +0300

    make the page responsive

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -20,17 +20,17 @@ const Docs = () => {
   }, [location]);
 
   return (
-    <div className="bg-bodyGradient h-screen">
+    <div className="bg-bodyGradient">
       <GlobalProvider>
         <Default>
-          <div className="grid grid-cols-5 h-full w-full pt-28 px-28 text-[13px]">
-            <div className="flex flex-col border-r border-custom-white overflow-y-auto">
+          <div className="grid grid-cols-5 h-full w-full pt-28 px-28 mdsm:pt-0 mdsm:pl-5 maxmd:pl-4 maxmd:pr-0 maxmd:grid-cols-6 mimd:px-14 text-[13px] maxx:px-14">
+            <div className="flex flex-col border-r border-custom-white overflow-y-auto mdsm:hidden maxmd:col-span-2">
               <LeftSidebar />
             </div>
-            <div className="flex flex-col border-r border-custom-white col-span-3 mx-8 text-white text-[14px] overflow-y-auto">
+            <div className="flex flex-col border-r mdsm:border-r-0 border-custom-white col-span-3 mdsm:col-span-6 maxmd:col-span-4 mx-8 maxmd:mr-0 maxmd:border-r-0 maxmd:ml-4 text-white text-[14px] overflow-y-auto mdsm:mx-0">
               <DocBody />
             </div>
-            <div className="text-white flex flex-col overflow-y-auto">
+            <div className="text-white flex flex-col overflow-y-auto mdsm:hidden maxmd:hidden">
               <RightSidebar />
             </div>
           </div>
