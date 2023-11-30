0bb7e23285c501e3f87264651d5cd0a296b76124
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-30 22:49:13 +0300

    apply the smx and sms breakpoints

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -3,8 +3,10 @@ import Slogan from "./Slogan";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col text-center justify-center text-custom-pink -mt-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-full ">
-      <p className="blur-xl text-[400px] font-sans md:text-2xl maxsm:text-[300px]">cjx</p>
+    <div className="flex flex-col text-center justify-center text-custom-pink left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-full ">
+      <p className="blur-xl text-[400px] font-sans md:text-2xl maxsm:text-[300px] smx:text-[250px] sms:text-[150px]">
+        cjx
+      </p>
       <Slogan />
     </div>
   );
