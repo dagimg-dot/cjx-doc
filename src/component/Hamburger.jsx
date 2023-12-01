ad308da952d349d89210b899b2338c3b1a1af852
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-01 14:21:11 +0300

    fix style for mobile screen

--- src/component/Hamburger.jsx
+++ src/component/Hamburger.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { FaBars } from 'react-icons/fa'
-const Hamburger = ({onClick}) => {
+import { FaBars } from "react-icons/fa";
+const Hamburger = ({ onClick }) => {
   return (
-    <div className='hidden mdsm:block'>
-    <FaBars className='text-2xl text-lime-500 mx-5 my-10' onClick={onClick}/>
+    <div className="hidden mdsm:flex">
+      <FaBars
+        className="text-2xl text-lime-500 mx-5 my-4 mdsm:mx-0"
+        onClick={onClick}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Hamburger
+export default Hamburger;
