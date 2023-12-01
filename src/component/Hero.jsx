dc87fed9315d22eb5ecd9becf44212d9006ab75e
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-01 14:21:23 +0300

    fix bug in breakpoint change

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -4,7 +4,7 @@ import Slogan from "./Slogan";
 const Hero = () => {
   return (
     <div className="flex flex-col text-center justify-center text-custom-pink left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-full ">
-      <p className="blur-xl text-[400px] font-sans md:text-2xl maxsm:text-[300px] smx:text-[250px] sms:text-[150px]">
+      <p className="blur-xl text-[400px] font-sans maxsm:text-[300px] smx:text-[250px] sms:text-[150px] -mt-10">
         cjx
       </p>
       <Slogan />
