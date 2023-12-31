c54abdd442bafde3c1c80da4ecd3fbf9521830e9
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-31 15:42:18 +0300

    refactor: full migration from context to route based

--- src/component/RightSidebar.jsx
+++ src/component/RightSidebar.jsx
@@ -1,16 +1,17 @@
-import useHash from "../hooks/useHash";
+import { useLocation } from "react-router-dom";
+import { onThisPageTopics } from "../utils/data";
+import cleanStr from "../utils/cleanStr";
 
 const RightSidebar = () => {
-  const [currentPage, parent] = useHash();
-  const subTopics = parent.children[currentPage].sub_children;
-  const subChildren = Object.keys(subTopics);
+  const { pathname } = useLocation();
+  const topics = onThisPageTopics[cleanStr(pathname.split("/").pop())];
 
   return (
     <div className="mx-2">
-      {subChildren.length > 0 ? (
+      {topics.length > 0 ? (
         <div className="flex flex-col">
           <h2 className="mb-4 font-bold text-lg">On this page</h2>
-          {subChildren.map((sub_child) => (
+          {topics.map((sub_child) => (
             <p
               className="my-2 px-2 border-l-2 border-custom-pink"
               key={sub_child}
