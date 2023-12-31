f0896727ee29f58ae9acf24524ba094863369945
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-31 12:14:04 +0300

    feat: add route to page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import Docs from "./pages/docs";
+import NotFound from "./pages/notfound";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Home />}></Route>
       <Route path="/contact" element={<Contact />}></Route>
       <Route path="/docs" element={<Docs />}></Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
