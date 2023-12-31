12461484efbfe540514fb4b3893641683885075c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-31 15:18:12 +0300

    fix: partial migration from context api to route based state

--- src/component/Submenu.jsx
+++ src/component/Submenu.jsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom";
-import useHash from "../hooks/useHash";
+import { Link, useLocation } from "react-router-dom";
+import DocPages from "../utils/docPages";
 
 const Submenu = ({ submenu_name }) => {
-  const [currentPage] = useHash();
+  const { pathname } = useLocation();
+
+  const findCategory = (submenuName) => {
+    for (const [category, pages] of Object.entries(DocPages)) {
+      if (submenuName in pages) {
+        return category;
+      }
+    }
+    return null;
+  };
+
+  const linkConstructor = () => {
+    const BASE_URL = "/docs";
+    const link =
+      BASE_URL + "/" + findCategory(submenu_name) + "/" + submenu_name;
+    const finalLink = link.replace(/\s+/g, "-");
+    return finalLink;
+  };
+
   const selectedColor =
-    submenu_name === currentPage
+    submenu_name === pathname.split("/").pop().replace(/-/g, " ")
       ? "pl-2 border-l-2 border-[#bcbcbc] text-[#bcbcbc]"
       : "";
 
   return (
     <button className={"cursor-pointer block " + selectedColor}>
-      <Link to={"#" + submenu_name}>{submenu_name}</Link>
+      <Link to={linkConstructor()}>{submenu_name}</Link>
     </button>
   );
 };
