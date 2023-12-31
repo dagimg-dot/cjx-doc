5dbf8354484e34eee5d731a86906ab41e2605d6a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-31 12:13:37 +0300

    feat: add page not found page

---
+++ src/pages/notfound.jsx
@@ -0,0 +1,16 @@
+import Button from "../component/Button";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="h-screen w-screen bg-bodyGradient flex flex-col gap-6 items-center text-bash-white">
+      <div className="text-9xl font-bold pt-20">404</div>
+      <div className="text-3xl">Page not found</div>
+      <Link to="/">
+        <Button>Home</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
