7e5fd0d1565546cff2d4de51f232cf6a1be2dacc
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-01-01 16:55:58 +0300

    fix: remove old code related to context api

--- src/utils/types.jsx
+++ src/utils/types.jsx
@@ -1,12 +1,7 @@
-const ACTION_TYPE = {
-  PARENT_CLICK: "PARENT_CLICK",
-  CHILD_CLICK: "CHILD_CLICK",
-};
-
 const PAGES = {
   DOCS: "docs",
   HOME: "home",
   CONTACT: "contact",
 };
 
-export { ACTION_TYPE, PAGES };
+export { PAGES };
